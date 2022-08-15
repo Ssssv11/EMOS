80e110cb084cc8086143ed23e7cd0698fd75a08e
Ssssv11 <1416431087@qq.com>
2022-08-16 01:16:31 +0800

    配置后端验证功能

--- src/main/java/com/hjc/controller/TestController.java
+++ src/main/java/com/hjc/controller/TestController.java
@@ -1,20 +1,24 @@
 package com.hjc.controller;
 
+import com.hjc.controller.form.TestSayHelloForm;
 import com.hjc.utils.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/test")
 @Api("测试Web接口")
 public class TestController {
 
-    @GetMapping("/sayHello")
+    @PostMapping("/sayHello")
     @ApiOperation("测试")
-    public R sayHello() {
-        return R.ok().put("message", "HelloWorld");
+    public R sayHello(@Valid @RequestBody TestSayHelloForm form) {
+        return R.ok().put("message", "Hello " + form.getName());
     }
 }
---
+++ src/main/java/com/hjc/controller/form/TestSayHelloForm.java
@@ -0,0 +1,20 @@
+package com.hjc.controller.form;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+/**
+ * Validation 后端验证 Form 封装类
+ */
+@ApiModel
+@Data
+public class TestSayHelloForm {
+    @NotBlank
+    @Pattern(regexp = "^[\\u4e00-\\u9fa5]{2,15}$", message = "不符合正则表达式")
+    @ApiModelProperty("姓名")
+    private String name;
+}
