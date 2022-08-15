a48dd41070897d4c3f595a840409e7143dfec9fc
Ssssv11 <1416431087@qq.com>
2022-08-16 00:47:50 +0800

    创建自定义异常类与Web通用返回对象R

---
+++ src/main/java/com/hjc/exception/EmosException.java
@@ -0,0 +1,31 @@
+package com.hjc.exception;
+
+import lombok.Data;
+
+@Data
+public class EmosException extends RuntimeException {
+    private String msg;
+    private int code = 500;
+
+    public EmosException(String msg) {
+        super(msg);
+        this.msg = msg;
+    }
+
+    public EmosException(String msg, Throwable e) {
+        super(msg, e);
+        this.msg = msg;
+    }
+
+    public EmosException(String msg, int code) {
+        super(msg);
+        this.msg = msg;
+        this.code = code;
+    }
+
+    public EmosException(String msg, int code, Throwable e) {
+        super(msg, e);
+        this.msg = msg;
+        this.code = code;
+    }
+}
