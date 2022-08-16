0ec1f355a7294f8e75c699404c3ed70f63aa35d7
Ssssv11 <1416431087@qq.com>
2022-08-16 13:17:57 +0800

    定义全局路径、封装ajax

--- emos-wx/main.js
+++ emos-wx/main.js
@@ -1,21 +1,46 @@
 import App from './App'
-
-// #ifndef VUE3
 import Vue from 'vue'
+
 Vue.config.productionTip = false
 App.mpType = 'app'
+
 const app = new Vue({
     ...App
 })
-app.$mount()
-// #endif
 
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+app.$mount()
+
+let baseUrl = "http://localhost:8999/emos-wx-api"
+Vue.prototype.url = {
+	register: baseUrl + "/user/register"
 }
-// #endif
+
+Vue.prototype.ajax = (url, method, data, fun) => {
+	uni.request({
+		"url": url,
+		"method": method,
+		"header": {
+			token: uni.getStorageSync('token')
+		},
+		"data": data,
+		success: (resp) => {
+			if(resp.statusCode == 401) {
+				uni.redirectTo({
+					url: 'pages/login/login.vue'
+				})
+			} else if(resp.statusCode == 200 && resp.data.code == 200) {
+				let data = resp.data
+				if(data.hasOwnProperty("token")) {
+					let token = data.token
+					uni.setStorageSync("token", token)
+				}
+				fun(resp)
+			} else {
+				uni.showToast({
+					icon: "none",
+					title: resp.data
+				})
+			}
+		}
+	})
+}
