ee72613b6833506d32e759e84a61fc1759cf9f19
Ssssv11 <1416431087@qq.com>
2022-08-16 21:23:15 +0800

    实现人脸签到持久层

--- src/main/java/com/hjc/db/dao/TbCheckinDao.java
+++ src/main/java/com/hjc/db/dao/TbCheckinDao.java
@@ -1,5 +1,6 @@
 package com.hjc.db.dao;
 
+import com.hjc.db.pojo.TbCheckin;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.HashMap;
@@ -7,4 +8,5 @@ import java.util.HashMap;
 @Mapper
 public interface TbCheckinDao {
     Integer haveCheckin(HashMap param);
+    void insert(TbCheckin checkin);
 }
--- src/main/java/com/hjc/db/dao/TbFaceModelDao.java
+++ src/main/java/com/hjc/db/dao/TbFaceModelDao.java
@@ -1,8 +1,11 @@
 package com.hjc.db.dao;
 
+import com.hjc.db.pojo.TbFaceModel;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TbFaceModelDao {
-
+    String searchFaceModel(int userId);
+    void insert(TbFaceModel faceModel);
+    int deleteFaceModel(int userId);
 }
